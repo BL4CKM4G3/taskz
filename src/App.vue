d04b90779c312b5ff68af3c9ba77e07ef38148b9
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-02-07 19:00:18 -0300

    feat: :sparkles: Improved main App component

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
+    <sidebar></sidebar>
+    <v-main class="grey darken-4">
+      <v-container>
+        <transition name="fade">
+          <router-view></router-view>
+        </transition>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Sidebar from "./components/Sidebar.vue"
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Sidebar
   },
 
   data: () => ({
@@ -58,3 +27,15 @@ export default {
   }),
 };
 </script>
+
+<style lang="scss">
+html::-webkit-scrollbar {
+  width: 7px;
+  background: #646464;
+}
+
+html::-webkit-scrollbar-thumb {
+  width: 7px;
+  background: #373737;
+}
+</style>
