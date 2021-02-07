d530ccfb151ce4a278c6bc7002c528d38f925196
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-02-07 19:00:33 -0300

    feat: :sparkles: Created icons Constant

---
+++ src/constants/icons.js
@@ -0,0 +1,18 @@
+import * as mdi from "@mdi/js";
+
+export const ICON_DASHBOARD = mdi.mdiViewDashboard;
+export const ICON_BRIEFCASE = mdi.mdiBriefcaseCheck;
+export const ICON_MENU = mdi.mdiMenu;
+export const ICON_CALENDAR = mdi.mdiCalendar;
+export const ICON_CHART = mdi.mdiChartPie;
+export const ICON_SETTINGS = mdi.mdiCogOutline;
+export const ICON_SEARCH = mdi.mdiMagnify;
+export const ICON_BELL = mdi.mdiBell;
+export const ICON_FAVOURITE = mdi.mdiStar;
+export const ICON_FAVOURITE_OUTLINE = mdi.mdiStarOutline;
+export const ICON_DOTS = mdi.mdiDotsVertical;
+export const ICON_PREV = mdi.mdiSkipPrevious;
+export const ICON_PLAY = mdi.mdiPlay;
+export const ICON_NEXT = mdi.mdiSkipNext;
+export const ICON_PAUSE = mdi.mdiPause;
+export const ICON_MESSAGE = mdi.mdiMessageOutline;
